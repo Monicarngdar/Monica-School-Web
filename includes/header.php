60bd19b41e8816cb7dff8f64ffa3570adfbcc452
Monicarngdar <monica.arong.darmanin@gmail.com>
2025-12-15 22:21:33 +0100

    Log in Form
    Fixed log in form and styling

--- includes/header.php
+++ includes/header.php
@@ -13,19 +13,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Myskolar Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet" >
 </head>
 
 <body>
 
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap z-3 main-header">
+    <header class="navbar navbar-dark sticky-top bg-myskolar flex-md-nowrap z-3 main-header">
         
         <div class="d-flex align-items-center navbar-brand-wrapper"> 
-            <a class="navbar-brand px-2" href="#">Myskolar</a>
+            <a class="navbar-brand px-2" href="#">MYSKOLAR</a>
             
-            <span class="page-subtitle">Dashboard</span>
-            
-            <span class="second-subtitle ">Email</span> 
+        <a class="nav-link text-white mx-3 fw-semibold" href="#">Dashboard</a>
+        <a class="nav-link text-white fw-semibold" href="#">Emails</a>
             
             </div>
         <div class="w-100 d-none d-md-block"></div>
@@ -42,17 +41,26 @@
     <div class="container-fluid">
         <div class="row">
             
-            <nav id="sidebarMenu" class="d-md-block bg-light sidebar collapse">
+            <nav id="sidebarMenu" class="d-md-block bg-skolar sidebar collapse">
                 <div class="position-sticky">
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Mn1</a>
+                            <a class="nav-link active" aria-current="page" >My Academics</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Mn2</a>
+                            <a class="nav-link" href="#">Timetable</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Mn3</a>
+                            <a class="nav-link" href="#">My Units</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Attendance</a>
+                        </li>
+                         <li class="nav-item">
+                            <a class="nav-link" href="#">Assignments</a>
+                        </li>
+                       <li class="nav-item">
+                            <a class="nav-link" href="#">Grades</a>
                         </li>
                     </ul>
                 </div>
--- login.php
+++ login.php
@@ -1,27 +1,68 @@
 
 <?php  include "includes/header.php";?>
 
+
 <!--- User Log in -->
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 main-content-wrapper">
 
-              <form action="includes/login-inc.php" method= "POST">
-<div class = "container">
-    <div class = "row">
-        <div class ="col">          
-            <label for="username">Username:</label>  
-            <input type="text" id="username" name="username">
-        </div>
+    <?php 
+        if(isset($_GET["error"])) { 
+            $message = "";
+            if ($_GET["error"] == "incorrectlogin"){
+                $message = "Incorrect Login";
+            }
+            ?>
+            <div class="row">
+                <div class="col"></div>
+                <div class="col border border-danger text-danger">
+                    <p><?php echo $message; ?></p>
+                </div>
+                <div class="col"></div>
+            </div>
+    <?php } ?>
 
-           <div class ="col">          
-            <label for="password">Password:</label>  
-            <input type="password" id="password" name="password">
-        </div>
-    </div>
+    <?php 
+        if(isset($_GET["error"])) { 
+            $message = "";
+            if ($_GET["error"] == "stmtfailed"){
+                $message = "error with sql query";
+            }
+            ?>
+            <div class="row">
+                <div class="col"></div>
+                <div class="col border border-danger text-danger">
+                    <p><?php echo $message; ?></p>
+                </div>
+                <div class="col"></div>
+            </div>
+    <?php } ?>
 
-    <div class = "row">
-        <div class ="col">          
-           <button type ="submit" name ="submit" id="submit">Login</button>
+    
+
+
+<div class = "login-container mx-auto">
+<h2 class="user-login text-center ">User Log in</h2>
+
+     <form action="includes/login-inc.php" method= "POST">
+
+     <div class = "mb-3"> 
+         <input type="text" id="username" name="username" class = "user-login-form" placeholder = "Username"> 
+         </div>
+
+     <div class = "mb-4"> 
+         <input type="text" id="password" name="password" class = "user-login-form" placeholder = "Password"> 
+        
+
+            </div>
+            </div>
+
+    
+<div class="d-flex justify-content-center gap-3">
+    <button type="button" class="btn btn-secondary">BACK</button>                
+     <button type="submit" class="btn btn-primary">LOG IN</button>
      </div>
+
+
   </div>
 </div>
 
